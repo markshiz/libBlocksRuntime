92e171b76ce3f77255b0f800cb4585cdfe2b38b4
Mark Schisler <mark.schisler@knowink.com>
2019-06-02 20:46:58 -0500

    Exporting header

--- Android.mk
+++ Android.mk
@@ -6,6 +6,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libblocksruntime
 
+LOCAL_EXPORT_C_INCLUDES := $(MY_INCLUDE_PATH)/Block.h
+
 LOCAL_SRC_FILES := runtime.c data.c
 
 LOCAL_CFLAGS := -DHAVE_CONFIG_H -I. -DBlocksRuntime_EXPORTS -DHAVE_SYNC_BOOL_COMPARE_AND_SWAP_INT -DHAVE_SYNC_BOOL_COMPARE_AND_SWAP_LONG -std=c99 -Wall -Wextra -W -pedantic -Wno-unused-parameter -fPIC -DPIC -nostdlibinc
